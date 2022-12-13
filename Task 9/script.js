78f274b62941def706c443a0912b94bbe5885ca4
Marius Palepšys <marius.palepsys@gmail.com>
2022-12-13 22:07:27 +0200

    Add fetch to task 9

--- Task 9/script.js
+++ Task 9/script.js
@@ -12,4 +12,33 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
+
+// =============== Fetc data =============== 
+
 const ENDPOINT = 'https://api.github.com/users';
+
+async function getData() {
+    try {
+        let response = await fetch(ENDPOINT);
+        if (response.status === 200){
+            return await response.json();
+        
+        }
+    }   catch(error){
+            console.error(error);
+    }
+}
+
+// =============== Fetc data =============== 
+
+let users = null;
+async function init() {
+    
+    users = [];
+    const data = await getData();
+    for(let i = 0; i < data.length; i++ ){   
+        users.push(data[i])
+    } 
+    console.log(users);
+}
+init()
