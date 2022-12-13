e394b35394e2288c3f75e0dd88ff9fb47313a9ed
Marius Palepšys <marius.palepsys@gmail.com>
2022-12-13 20:34:47 +0200

    completed task 2

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,3 +1,4 @@
+
 /* ------------------------------ TASK 8 --------------------------------------------
 Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri gebės sukurti objektus su 4 metodais:
 sum(a, b) - priima du skaičius ir grąžina jų sumą.
@@ -5,3 +6,29 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calc (a,b){
+    this.x = a;
+        this.y = b;
+    this.suma = function () {
+        let sum = this.x + this.y
+        return sum
+    };
+
+    this.substraction = function  () {
+        return this.a - this.b;
+    };
+    this.multiplication = function () {
+        return this.a * this.b
+    }
+    this.division = function  () {
+        return this.a / this.b
+    }
+}
+
+const digits = new Calc(8,2);
+
+console.log(digits.suma);
+console.log(digits.substraction);
+console.log(digits.multiplication);
+console.log(digits.division);
--- Task 2/script.js
+++ Task 2/script.js
@@ -9,3 +9,40 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+
+// ============== konstruktoriaus funkcija ==============
+
+function Movie (title,director,budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+
+// ============== funkcijos metodas ==============
+
+    this.wasExpensive = () => {
+       return this.budget > 100000000 ?  true :  false;
+        }
+    }
+
+// ============== konstruktoriaus panaudojimas ==============
+
+const newMovie = new Movie('Matrica', 'The Wachowskis', 63000000);
+const newMovie2 = new Movie('Titanic', 'James Cameron', 200000000);
+
+// ============== atvaizdavimas konsoleje ==============
+
+console.log(`${newMovie.title} was directed by ${newMovie.director} his budget was ${newMovie.budget} $ and it was expensive movie: ${(newMovie.wasExpensive())}.`);
+console.log(`${newMovie2.title} was directed by ${newMovie2.director} his budget was ${newMovie2.budget} $ and it was expensive movie: ${(newMovie2.wasExpensive())}.`);
+
+// ============== atvaizdavimas html ==============
+
+const matrix = `${newMovie.title} was directed by ${newMovie.director} his budget was ${newMovie.budget} $ and it was expensive movie: ${(newMovie.wasExpensive())}.`;
+const p = document.createElement('p');
+p.innerText = matrix;
+
+const titanic = `${newMovie2.title} was directed by ${newMovie2.director} his budget was ${newMovie2.budget} $ and it was expensive movie: ${(newMovie2.wasExpensive())}.`;
+const p2 = document.createElement('p');
+p2.innerText = titanic;
+
+document.body.append(p,p2);
