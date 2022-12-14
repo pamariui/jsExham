5730793877e3b05c4bfa0247973945b16c0d1e4a
Marius Palepšys <marius.palepsys@gmail.com>
2022-12-14 17:45:05 +0200

    complete task9

--- Task 9/script.js
+++ Task 9/script.js
@@ -22,23 +22,68 @@ async function getData() {
         let response = await fetch(ENDPOINT);
         if (response.status === 200){
             return await response.json();
-        
         }
     }   catch(error){
             console.error(error);
     }
-}
+};
 
-// =============== Fetc data =============== 
+// =============== variables =============== 
 
 let users = null;
-async function init() {
-    
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+// =============== function's =============== 
+
+// ===== function to put data to array =====
+
+async function renderData() {
     users = [];
     const data = await getData();
+
     for(let i = 0; i < data.length; i++ ){   
-        users.push(data[i])
-    } 
-    console.log(users);
-}
-init()
+        users.push(data[i]);
+    };
+
+    const sorted = users.sort((a,b) => {
+        if (a.login.toUpperCase() > b.login.toUpperCase()) {
+            return -1;
+        }});
+
+    createCard(sorted);
+};
+
+// ===== function to create DOM card element for array ====
+
+function createCard(array) {
+    array.forEach(el => {
+
+        const login = document.createElement('h3');
+        login.innerText = el.login;
+
+        const img = document.createElement('img');
+        img.src = el.avatar_url;
+        img.setAttribute('alt', 'UserPicture');
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.append(login,img);
+        output.append(card);
+    });
+       
+};
+
+// ===== create button event listener =====
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+    users = [];
+
+    if(btn.classList.contains('active')){
+        output.innerHTML = "";
+        renderData();
+    } else {
+        output.innerHTML = `Press "Show Users" button to see users`;
+    }
+});
