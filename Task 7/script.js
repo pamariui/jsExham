0091b54d757f6537b5f44ddc540035b2e142170d
Marius Palepšys <marius.palepsys@gmail.com>
2022-12-15 18:37:38 +0200

    Complete task7 Finaly!

--- Task 7/script.js
+++ Task 7/script.js
@@ -9,4 +9,75 @@ Turime gauti naujus duomenis.
 Hint: naudojamas localstorage, saugoti informacijai;
 ------------------------------------------------------------------------------------ */
 
-const URL = "https://random-d.uk/api/random";
+// =============== api fetch ===============
+
+const URL = "https://cors-anywhere.herokuapp.com/https://random-d.uk/api/random";
+
+async function getData() {
+    try {
+        let response = await fetch(URL);
+        if (response.status === 200){
+            return await response.json();
+        }
+    }   catch(error){
+            console.error(error);
+    }
+};
+
+
+// =============== variables ===============
+
+const output = document.getElementById('output');
+const btn = document.getElementById('btn');
+let duck = {};
+
+// =============== functions ===============
+
+// ===== Funckcija generuoja naujas antis bei iraso jas i local =====
+
+async function renderData() {
+
+    const data = await getData();
+    localStorage.setItem('url', data.url)
+    localStorage.setItem('message', data.message)
+    quackGenerator(data);
+};
+
+// ===== atspausdina gautus duomenis =====
+const quackGenerator = (data) => {
+    const img = document.createElement('img');
+    img.src = data.url;
+
+    const message = document.createElement('h3');
+    message.innerText = data.message;
+    output.append(img,message);
+}
+
+
+//  ===== paleidus puslapi patikrina ar kas nors yra local storage jei ne generuoja nauja antuka =====
+const init = () => {
+
+    if(localStorage.getItem('url')){
+
+        const img = document.createElement('img');
+        img.src = localStorage.getItem('url');
+
+        const message = document.createElement('h3');
+        message.innerText = localStorage.getItem('message')
+        output.append(img,message);
+        console.log(localStorage.getItem('url'));
+    } else {
+        renderData();
+    }
+}
+
+
+// =============== initialize ===============
+
+init ()
+
+// ===== paspaudus mygtuka uzkrauna nauja antuka =====
+btn.addEventListener('click', () => {
+    output.innerText = ""
+    renderData()
+} )
